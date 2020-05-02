9d34e99a46a628db4b62ca0e0e6234a89b2c45e3
hevalhazalkurt <hevalhazal@gmail.com>
2020-05-02 16:13:44 +0300

    Created profile model for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #fullname = models.OneToOneField(User, on_delete=models.CASCADE, blank = True)
+    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+    cover = models.ImageField(default="defaultcover.jpg", upload_to="cover_pics")
+    about = models.CharField(max_length=250, blank = True)
+    linkedin = models.CharField(max_length=250, blank = True)
+    github = models.CharField(max_length=250, blank = True)
+    twitter = models.CharField(max_length=250, blank = True)
+    instagram = models.CharField(max_length=250, blank = True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
