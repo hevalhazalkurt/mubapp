eff79558361d766cc8f5460697ac43011509f8d2
hevalhazalkurt <hevalhazal@gmail.com>
2020-05-02 20:44:24 +0300

    Added rich text editor upload path

--- mubapp/urls.py
+++ mubapp/urls.py
@@ -17,7 +17,9 @@ urlpatterns = [
     path('user/password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),
     path('user/password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
     path('user/password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name="password_reset_complete"),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
