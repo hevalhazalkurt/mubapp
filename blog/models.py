46cd2df8983406566a887360df0661184823098e
hevalhazalkurt <hevalhazal@gmail.com>
2020-05-02 16:17:29 +0300

    Created post and post category models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+
+
+class PostCategory(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'post category'
+        verbose_name_plural = 'post categories'
+
+
+    def __str__(self):
+        return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('blog-category', args=[self.slug])
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(title)
+        super().save(*args, **kwargs)
+
+
+
+class Post(models.Model):
+    image = models.ImageField(upload_to="images/")
+    title = models.CharField(max_length=150)
+    summary = models.CharField(max_length=250)
+    category = models.ForeignKey(PostCategory, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=250, unique=True, blank = True)
+
+
+
+    def __str__(self):
+        return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('blog-detail', args=[self.slug])
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
